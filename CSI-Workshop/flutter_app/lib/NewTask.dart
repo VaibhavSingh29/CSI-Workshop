c817a10f340c81b8e9457aeb0ec285bb03038605
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2020-10-08 12:37:24 +0530

    test

---
+++ CSI-Workshop/flutter_app/lib/NewTask.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class NewTask extends StatefulWidget {
+  @override
+  _NewTaskState createState() => _NewTaskState();
+}
+
+class _NewTaskState extends State<NewTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ CSI-Workshop/flutter_app/lib/Task.dart
@@ -0,0 +1,38 @@
+class Task
+{
+  final String task_name, description, type, time;
+
+  Task(this.task_name, this.description, this.type, this.time);
+}
+
+class Task_list{
+
+  static List<Task> task_list=[];
+
+  static List get_list()
+  {
+    return task_list;
+  }
+
+  static add_to_list(task)
+  {
+    task_list.add(task);
+  }
+
+  static remove_item(index){
+    task_list.removeAt(index);
+  }
+
+  static get_task_info(index){
+    Task task=task_list[index];
+    Map m={
+      "task": task.task_name,
+      "task_description": task.description,
+      "task_type": task.type,
+      "task_time": task.time
+    };
+
+    return m;
+  }
+
+}
