647db56bcb3b0651e22c7675b66e88b26c3e9a46
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2020-10-08 12:34:35 +0530

    first page done

--- CSI-Workshop/flutter_app/lib/main.dart
+++ CSI-Workshop/flutter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/NewTask.dart';
+import 'package:flutter_app/Task.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,93 +28,80 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  List list=["a", "b"];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+
+      body: Column(
+        children: [
+         Stack(
+           children: [
+             Container(
+               height: 200,
+               width: MediaQuery.of(context).size.width,
+               color: Colors.greenAccent,
+             ),
+             Text('Hello there!!'),
+
+           ],
+         ),
+          Text('Tasks to complete 12'),
+          Container(
+            height: 400,
+            child: ListView.builder(
+              itemCount: Task_list.get_list().length,
+              itemBuilder: (context, index){
+                return Dismissible(
+                  key: Key(Task_list.get_list().toString()),
+                  onDismissed: (direction){
+                    setState(() {
+                      Task_list.remove_item(index);
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text("Item deleted"),)
+                      );
+                    });
+                  },
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                       Text( Task_list.get_task_info(index)['task'].toString()),
+                      ],
+                    ),
+                    subtitle: Column(
+                      children: [
+                        Text( Task_list.get_task_info(index)['task_description'].toString()),
+                        Text( Task_list.get_task_info(index)['task_type'].toString()),
+                        Text( Task_list.get_task_info(index)['task_time'].toString()),
+
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.pinkAccent,
+        splashColor: Colors.purple,
+        onPressed: (){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>NewTask()));
+        },
+      ),
     );
   }
 }
